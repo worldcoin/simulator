cb4ed4fd8b7b5f1d68732ab221a57684e8650499
Paolo D'Amico <paolodamico@users.noreply.github.com>
2022-06-09 14:19:16 +0200

    refactor: support new QR codes

--- src/common/helpers/validate-wallet-uri.ts
+++ src/common/helpers/validate-wallet-uri.ts
@@ -1,19 +1,56 @@
 import {
-  parseWalletConnectUri,
   isWalletConnectSession,
+  parseWalletConnectUri,
 } from "@walletconnect/utils";
 
-export const validateWalletUri = (
-  data: string,
-): { valid: boolean; message: string } => {
-  const uri = parseWalletConnectUri(data);
+interface ParseWorldIDQRCodeOutput {
+  valid: boolean;
+  errorMessage?: string;
+  uri?: string;
+}
 
-  if (uri.protocol !== "wc" || !isWalletConnectSession(uri)) {
+export const parseWorldIDQRCode = (data: string): ParseWorldIDQRCodeOutput => {
+  if (data.startsWith("https://worldcoin.org/verify")) {
+    // New version of World ID QR code
+    const parsedUrl = new URL(data);
+    const key = parsedUrl.searchParams.get("k");
+    const bridge = parsedUrl.searchParams.get("b");
+    const handshakeTopic = parsedUrl.searchParams.get("t");
+    const version = parsedUrl.searchParams.get("v");
+
+    console.log(key, bridge, handshakeTopic, version);
+
+    if (!key || !bridge || !handshakeTopic || !version) {
+      return {
+        valid: false,
+        errorMessage: "Improperly formed World ID QR code. Parameters missing.",
+      };
+    }
+
+    const encodedBridge = encodeURIComponent(`https://${bridge}`);
     return {
-      valid: false,
-      message: `Not a WalletConnect session URI: ${data}`,
+      uri: `wc:${handshakeTopic}@${version}?bridge=${encodedBridge}&key=${key}`,
+      valid: true,
     };
   }
 
-  return { valid: true, message: "Success" };
+  // Legacy version of QR code
+  // TODO: Legacy support should be removed after August 31
+  const parsedUri = parseWalletConnectUri(data);
+
+  if (parsedUri.protocol !== "wc" || !isWalletConnectSession(parsedUri)) {
+    return {
+      valid: false,
+      errorMessage: `Not a WalletConnect session URI: ${data}`,
+    };
+  }
+
+  console.warn(
+    "DEPRECATED. This QR code version is deprecated and support will be removed soon. Please upgrade the JS widget to version 0.0.3 or above.",
+  );
+  alert(
+    "This QR code version is deprecated and support will be removed soon. Please upgrade the JS widget to version 0.0.3 or above.",
+  );
+
+  return { valid: true, uri: data };
 };
