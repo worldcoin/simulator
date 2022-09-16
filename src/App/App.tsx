cdd6327620bd30d3557a480ea1ac57cf5c60555f
Denis Bushaev <denis.bushaev@ottofeller.com>
2022-09-16 17:43:18 +0300

    additional remove old code

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -189,15 +189,6 @@ const App = React.memo(function App() {
             />
           )}
 
-          {phase === Phase.VerifyIdentity && (
-            <VerifyIdentity
-              extended={extendedVerifyIdentity}
-              setPhase={setPhase}
-              className="z-30 pt-3.5 pb-6 xs:pb-0"
-              setVerificationSkipped={setVerificationSkipped}
-              identity={identity}
-            />
-          )}
           <hr
             className={cn(
               "mb-2 hidden h-1 w-full max-w-[134px] justify-self-center rounded-full border-none bg-000000/20",
@@ -206,8 +197,6 @@ const App = React.memo(function App() {
           />
         </section>
       )}
-
-      {phase === Phase.IdentityFaucet && <IdentityFaucet />}
     </div>
   );
 });
