c8b2727ac34b153fae41bd9d009d7e2644b8d8a4
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-02-10 11:11:53 +0100

    new URL construction

--- src/common/helpers/validate-wallet-uri.ts
+++ src/common/helpers/validate-wallet-uri.ts
@@ -7,20 +7,15 @@ interface ParseWorldIDQRCodeOutput {
 export const parseWorldIDQRCode = (data: string): ParseWorldIDQRCodeOutput => {
   // New version of World ID QR code
   const parsedUrl = new URL(data);
-  const topic = parsedUrl.searchParams.get("t");
-  const version = parsedUrl.searchParams.get("v");
-  const relay = parsedUrl.searchParams.get("p");
-  const key = parsedUrl.searchParams.get("k");
+  const uri = parsedUrl.searchParams.get("w");
+  const wcRegex = /^wc:[A-Za-z0-9]+@2/;
 
-  if (!topic || !version || !relay || !key) {
-    return {
-      valid: false,
-      errorMessage: "Improperly formed World ID QR code. Parameters missing.",
-    };
+  if (!uri || !uri.match(wcRegex)) {
+    return { valid: false };
   }
 
   return {
-    uri: `wc:${topic}@${version}?relay-protocol=${relay}&symKey=${key}`,
+    uri,
     valid: true,
   };
 };
