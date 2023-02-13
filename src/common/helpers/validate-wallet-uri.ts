3af24337dbf0779ca5e7a713078441395419cb07
Igor Osipov <igor.osipov@ottofeller.com>
2023-02-13 15:14:31 +0100

    fix decodinng in parseWorldIDQRCode

--- src/common/helpers/validate-wallet-uri.ts
+++ src/common/helpers/validate-wallet-uri.ts
@@ -6,7 +6,8 @@ interface ParseWorldIDQRCodeOutput {
 
 export const parseWorldIDQRCode = (data: string): ParseWorldIDQRCodeOutput => {
   // New version of World ID QR code
-  const parsedUrl = new URL(data);
+  const decodedData = decodeURIComponent(data);
+  const parsedUrl = new URL(decodedData);
   const uri = parsedUrl.searchParams.get("w");
   const wcRegex = /^wc:[A-Za-z0-9]+@2/;
 
