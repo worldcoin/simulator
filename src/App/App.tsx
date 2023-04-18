c2ee714860479d6e4bc19ea4d14a85340dc3cea6
Michael Furutin <michael.furutin@ottofeller.com>
2023-04-18 15:06:57 +0300

    style: change layout paddings

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -120,11 +120,11 @@ const App = React.memo(function App() {
 
       <section
         className={cn(
-          "h-full w-full p-4 pb-8 xs:h-[812px] xs:w-[375px] xs:overflow-hidden xs:rounded-40 xs:border-4 xs:border-183c4a",
+          "h-full w-full p-4 xs:h-[812px] xs:w-[375px] xs:overflow-hidden xs:rounded-40 xs:border-4 xs:border-183c4a",
           "relative col-start-1 row-start-1 grid grid-rows-auto/1fr self-center justify-self-center",
           "gap-y-3 bg-ffffff transition duration-500 dark:bg-0c0e10 xs:[zoom:.9] md:[zoom:initial]",
           { "xs:grid-rows-auto/1fr/auto": phase !== Phase.Identity },
-          { "pb-7 xs:grid-rows-auto/1fr": phase === Phase.Identity },
+          { "xs:grid-rows-auto/1fr": phase === Phase.Identity },
         )}
       >
         <header className="z-10 grid grid-cols-1fr/auto px-4">
