006e54c8343639b64887724b30dbef7eca0a7646
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-04-21 12:28:33 +0200

    Update identity.ts

--- src/lib/identity.ts
+++ src/lib/identity.ts
@@ -2,7 +2,7 @@ import { Identity } from "@semaphore-protocol/identity";
 
 /**
  * Generate a Semaphore identity. Can be either fully random or based on a seed message.
- * @param seed
+ * @param seed For deterministic identities. Used with a seed phrase or signature from a wallet.
  * @returns
  */
 export const generateIdentity = (seed?: string): Identity => {
