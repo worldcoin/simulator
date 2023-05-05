64bf50000220e6efcfbf6d7c8d668706ee906360
Max Petretta <git@maxpetretta.com>
2023-05-05 14:05:44 -0400

    update sequencer service to use new endpoints

--- src/services/sequencer.ts
+++ src/services/sequencer.ts
@@ -1,64 +1,87 @@
-import { Environment } from "@/types";
+import type {
+  InclusionProofResponse,
+  InsertIdentityResponse,
+  SequencerRequest,
+} from "@/types";
+import { CredentialType } from "@/types";
 
-type EncodedCommitment = string; // this should be a 64-padded hex-string
-
-const SEQUENCER_PASSWORD = process.env.SEQUENCER_PASSWORD;
-
-interface InclusionProofResponse {
-  root: EncodedCommitment;
-  proof: Record<"Left" | "Right", string>[];
-}
-
-const SEQUENCER_ENDPOINT: Record<Environment, string> = {
-  [Environment.STAGING]: "https://signup.stage-crypto.worldcoin.dev/",
-  [Environment.PRODUCTION]: "https://signup.crypto.worldcoin.dev/",
+const SEQUENCER_PASSWORD: Record<CredentialType, string | undefined> = {
+  [CredentialType.Orb]: process.env.ORB_SEQUENCER_PASSWORD,
+  [CredentialType.Phone]: process.env.PHONE_SEQUENCER_PASSWORD,
 };
 
-const getUrl = (pathname: string, env: Environment) =>
-  new URL(pathname, SEQUENCER_ENDPOINT[env]);
+const SEQUENCER_ENDPOINT: Record<CredentialType, string> = {
+  [CredentialType.Orb]: "https://signup-v2.stage-crypto.worldcoin.dev/",
+  [CredentialType.Phone]: "https://phone-signup-v2.stage-crypto.worldcoin.dev/",
+};
 
-async function postRequest<T = unknown>(
-  endpoint: string,
-  identityCommitment: EncodedCommitment,
-  env: Environment,
-  authenticateRequest?: boolean,
-) {
-  if (authenticateRequest && !SEQUENCER_PASSWORD) {
-    throw "Sequencer password is not provided and this request requires authentication. Please set `SEQUENCER_PASSWORD` env var.";
-  }
-  const res = await fetch(getUrl(endpoint, env).toString(), {
-    method: "POST",
-    body: JSON.stringify([1, identityCommitment]),
-    headers: {
-      "Content-Type": "application/json",
-      ...(authenticateRequest
-        ? { Authorization: `Basic ${btoa(`worldcoin:${SEQUENCER_PASSWORD}`)}` }
-        : {}),
-    },
-  });
-  if (!res.ok)
-    throw new TypeError(
-      `Failed to call ${endpoint} [${res.status}]: ${res.statusText}`,
-    );
-  return (await res.json()) as Promise<T>;
+function buildUrl(pathname: string, credentialType: CredentialType) {
+  return new URL(pathname, SEQUENCER_ENDPOINT[credentialType]);
 }
 
-export async function insertIdentity(
-  identityCommitment: EncodedCommitment,
-  env: Environment = Environment.STAGING,
-) {
-  return await postRequest<{
-    identityIndex: number;
-  }>("insertIdentity", identityCommitment, env, true);
+function buildHeaders(
+  authenticate: boolean,
+  credentialType: CredentialType,
+): HeadersInit {
+  const headers: HeadersInit = {
+    "Content-Type": "application/json",
+  };
+
+  if (authenticate) {
+    headers.Authorization = `Basic ${btoa(
+      `worldcoin:${SEQUENCER_PASSWORD[credentialType]}`,
+    )}`;
+  }
+  return headers;
+}
+
+async function postRequest<T = unknown>(request: SequencerRequest): Promise<T> {
+  if (request.authenticate && !SEQUENCER_PASSWORD[request.credentialType]) {
+    throw new Error(
+      `Sequencer password for '${request.credentialType}' is not provided and this request requires authentication. Please set SEQUENCER_PASSWORD environment variables.`,
+    );
+  }
+
+  const response = await fetch(
+    buildUrl(request.endpoint, request.credentialType).toString(),
+    {
+      method: "POST",
+      body: JSON.stringify({ identityCommitment: request.commitment }),
+      headers: buildHeaders(
+        request.authenticate ?? false,
+        request.credentialType,
+      ),
+    },
+  );
+
+  if (!response.ok) {
+    throw new Error(
+      `Failed to call ${request.endpoint} on sequencer for '${request.credentialType}', [${response.status}]: ${response.statusText}`,
+    );
+  }
+
+  return (await response.json()) as Promise<T>;
 }
 
 export async function inclusionProof(
-  identityCommitment: EncodedCommitment,
-  env: Environment = Environment.STAGING,
+  commitment: string,
+  credentialType: CredentialType,
 ) {
-  return await postRequest<InclusionProofResponse>(
-    "inclusionProof",
-    identityCommitment,
-    env,
-  );
+  return await postRequest<InclusionProofResponse>({
+    endpoint: "inclusionProof",
+    commitment,
+    credentialType,
+  });
+}
+
+export async function insertIdentity(
+  commitment: string,
+  credentialType: CredentialType,
+) {
+  return await postRequest<InsertIdentityResponse>({
+    endpoint: "insertIdentity",
+    commitment,
+    credentialType,
+    authenticate: true,
+  });
 }
