421ad46dcdfbb592c54ead657707df7ae8d3c4dd
Miguel Piedrafita <github@miguel.build>
2023-07-12 15:09:41 +0200

    Drag to dismiss drawer

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,7 +1,8 @@
+import { cn } from "@/lib/utils";
 import { Content, Overlay, Root } from "@radix-ui/react-dialog";
-import clsx from "clsx";
+import type { PanInfo } from "framer-motion";
 import { AnimatePresence, motion } from "framer-motion";
-import React from "react";
+import React, { useRef } from "react";
 
 export interface DrawerProps {
   open: boolean;
@@ -12,6 +13,17 @@ export interface DrawerProps {
 }
 
 export const Drawer = React.memo(function Drawer(props: DrawerProps) {
+  const panel = useRef<HTMLDivElement>(null);
+
+  const handleDragEnd = (_: DragEvent, info: PanInfo) => {
+    if (!panel.current) return;
+
+    const { velocity, offset } = info;
+    const { height } = panel.current.getBoundingClientRect();
+
+    if (velocity.y > 20 || offset.y > height / 2) props.onClose();
+  };
+
   return (
     <Root
       open={props.open}
@@ -37,11 +49,16 @@ export const Drawer = React.memo(function Drawer(props: DrawerProps) {
         {props.open && (
           <Content forceMount>
             <motion.div
+              ref={panel}
+              dragElastic={0.05}
+              dragConstraints={{ top: 0 }}
+              drag="y"
+              onDragEnd={handleDragEnd}
               initial={{ y: "100%" }}
               animate={{ y: 0 }}
               exit={{ y: "120%" }}
               transition={{ type: "spring", stiffness: 2000, damping: 120 }}
-              className={clsx(
+              className={cn(
                 "absolute inset-x-0 bottom-0 z-30 rounded-t-20 bg-white p-6 outline-none",
                 {
                   "top-[44px]": props.fullHeight,
