c58778db3b25fd607109aba172acefe14a005416
igorosip0v <igor.osipov@ottofeller.com>
2023-08-09 15:57:50 +0200

    fix: add missing csp header values

--- next.config.js
+++ next.config.js
@@ -27,11 +27,22 @@ const nextConfig = {
             "connect-src": [
               "'self'",
               "wss://relay.walletconnect.com",
+              "wss://www.walletlink.org/rpc",
+              "wss://*.bridge.walletconnect.org",
+              "https://app.posthog.com",
+              "https://status.worldcoin.org",
+            ],
+            "script-src": [
+              "'self'",
+              "'unsafe-eval'",
+              "https://storage.googleapis.com",
+              "'sha256-JPvpq7njEeX0V6q+ifkLzYVVK7l/7R+QtUoSdPw7Zdo='",
               "https://app.posthog.com",
             ],
-            "script-src": ["'self'", "https://app.posthog.com"],
+            "frame-src": ["'self'", "https://verify.walletconnect.com/"],
           },
           permissionsPolicy: {
+            camera: `self`,
             "clipboard-write": `self`,
           },
         }),
