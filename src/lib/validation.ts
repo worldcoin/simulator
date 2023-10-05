2214f95cd46d0485b1db4075cbd532c4c68b94f7
0xPenryn <penryn@pm.me>
2023-10-05 14:10:39 -0500

    fix: QR code parsing and improve validation

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -5,7 +5,26 @@ import { buffer_decode } from "./utils";
 export function validateWorldIDQRCode(data: string): boolean {
   const url = new URL(data);
 
-  return url.searchParams.get("t") == "wld" && url.searchParams.has("k");
+  let valid = false;
+
+  const t = url.searchParams.get("t");
+  const k = url.searchParams.get("k");
+  const i = url.searchParams.get("i");
+
+  if (
+    t == "wld" &&
+    k &&
+    i &&
+    /* cSpell:disable */
+    /^[A-Za-z0-9+/]{42}[AEIMQUYcgkosw048]=$/.test(k) &&
+    /* cSpell:enable */
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/.test(
+      i,
+    )
+  ) {
+    valid = true;
+  }
+  return valid;
 }
 
 export async function parseWorldIDQRCode(
@@ -14,6 +33,7 @@ export async function parseWorldIDQRCode(
   const url = new URL(data);
   const requestId = url.searchParams.get("i");
   const encodedKey = url.searchParams.get("k");
+  const bridgeUrl = url.searchParams.get("b");
 
   if (url.searchParams.get("t") != "wld" || !requestId || !encodedKey) {
     return { valid: false };
@@ -25,12 +45,12 @@ export async function parseWorldIDQRCode(
       valid: true,
       key: await window.crypto.subtle.importKey(
         "raw",
-        buffer_decode(decodeURIComponent(encodedKey)),
+        buffer_decode(encodedKey),
         { name: "AES-GCM" },
         false,
         ["encrypt", "decrypt"],
       ),
-      bridgeUrl: url.searchParams.get("b"),
+      bridgeUrl,
     };
   } catch (e) {
     console.error(e);
