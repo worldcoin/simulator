417e7ef1834d08f63390f7b8e440f9a3f80f24bc
Andrew Wang <andy.wang@Andrew-Wang.local>
2023-12-12 09:42:03 +0100

    fixes besides crashing and refactoring errors

--- src/components/Modal/ModalError.tsx
+++ src/components/Modal/ModalError.tsx
@@ -1,5 +1,4 @@
 import { Icon } from "../Icon";
-import ModalEnvironment from "./ModalEnvironment";
 
 export default function ModalError(props: {
   errorCode: string;
@@ -23,13 +22,35 @@ export default function ModalError(props: {
           className="h-10 w-10 text-white"
           bgClassName="h-20 w-20 rounded-full bg-gray-400"
         />
-        <h2 className="mt-4 text-h2 font-bold">Expired QR Code</h2>
-        <p className="mt-4 text-center">
-          This code has expired <br></br> Please try to use a different one.
+        <h2 className="mt-4 bg-gray-900 text-h2 font-bold">Expired QR Code</h2>
+        <p className="mt-4 bg-gray-500 text-center">
+          This connection has expired <br></br> Please try again.
         </p>
       </div>
     );
   } else {
-    return <ModalEnvironment />;
+    return (
+      <div className="flex h-[360px] flex-col items-center justify-center">
+        <button
+          className="absolute right-5 top-5 flex w-full justify-end"
+          onClick={props.close}
+        >
+          <Icon
+            name="close"
+            className="h-6 w-6 text-black"
+            bgClassName="h-9 w-9 rounded-full bg-gray-200"
+          />
+        </button>
+        <Icon
+          name="warning"
+          className="h-10 w-10 text-white"
+          bgClassName="h-12 w-12 rounded-full bg-gray-400"
+        />
+        <h2 className="mt-4 bg-gray-900 text-h2 font-bold">Error</h2>
+        <p className="mt-4 bg-gray-500 text-center">
+          Something went wrong <br></br> Please try again later.
+        </p>
+      </div>
+    );
   }
 }
