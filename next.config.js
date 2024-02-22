435e6734d70a219fc8c513dd1468f3dac440b75d
0xPenryn <ian.dilick@toolsforhumanity.com>
2024-02-22 13:35:48 -0600

    fix: permit world-id-assets.com for images

--- next.config.js
+++ next.config.js
@@ -12,6 +12,12 @@ const nextConfig = {
         port: "",
         pathname: "**",
       },
+      {
+        protocol: "https",
+        hostname: "world-id-assets.com",
+        port: "",
+        pathname: "**",
+      },
     ],
   },
 
